c0a2ce92ad51b97a59af5ee9c173631be661ef27
jixiaoyong <jixiaoyong1995@gmail.com>
2022-04-18 15:14:37 +0800

    feat: add animation effect to ExpandedIconButton

--- lib/widgets/expanded_icon_button.dart
+++ lib/widgets/expanded_icon_button.dart
@@ -9,7 +9,7 @@ import 'package:sirc/utils/size_extension.dart';
 * @email: jixiaoyong1995@gmail.com
 * @date: 22/04/18
 */
-class ExpandedIconButton extends StatelessWidget {
+class ExpandedIconButton extends StatefulWidget {
   final IconData icon;
   final VoidCallback onPressed;
   final double height;
@@ -34,48 +34,114 @@ class ExpandedIconButton extends StatelessWidget {
   }
 
   @override
+  State<ExpandedIconButton> createState() => _ExpandedIconButtonState();
+}
+
+class _ExpandedIconButtonState extends State<ExpandedIconButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  double get _width =>
+      widget.isExpanded == true ? widget.maxWidth : widget.minWidth!;
+  late double _trueWidth;
+
+  @override
+  void initState() {
+    var minWidth = widget.minWidth!;
+
+    _trueWidth = widget.height;
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    );
+
+    _controller.addListener(() {
+      setState(() {
+        _trueWidth = (_width - minWidth) * _controller.value + minWidth;
+        if (_trueWidth > widget.maxWidth) {
+          _trueWidth = widget.maxWidth;
+        }
+      });
+    });
+
+    if (widget.isExpanded == true) {
+      _controller.forward();
+    }
+
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ExpandedIconButton oldWidget) {
+    if (oldWidget.isExpanded != widget.isExpanded) {
+      if (widget.isExpanded == true) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: height,
-      width: isExpanded == true ? maxWidth : minWidth,
-      padding:
-          isExpanded == true ? EdgeInsets.all(0.1 * height) : EdgeInsets.zero,
+      height: widget.height,
+      width: _trueWidth,
+      padding: widget.isExpanded == true
+          ? EdgeInsets.all(0.1 * widget.height)
+          : EdgeInsets.zero,
       decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: colors,
+            colors: widget.colors,
             begin: Alignment.centerLeft,
             end: Alignment.centerRight,
           ),
-          borderRadius: BorderRadius.circular(height / 2)),
+          borderRadius: BorderRadius.circular(widget.height / 2)),
       child: GestureDetector(
-        onTap: onPressed,
-        child: Row(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
+        onTap: widget.onPressed,
+        child: Stack(
           children: <Widget>[
-            if (isExpanded == true)
-              Padding(
-                padding: EdgeInsets.only(left: 0.5 * height),
-                child: Text(
-                  text,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.sp,
+            if (widget.isExpanded == true)
+              Positioned(
+                left: 0.5 * widget.height,
+                child: Container(
+                  height: widget.height,
+                  padding: EdgeInsets.only(bottom: 0.1 * widget.height),
+                  alignment: Alignment.center,
+                  child: Text(
+                    widget.text,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.sp,
+                    ),
                   ),
                 ),
               ),
-            if (isExpanded == true) const Spacer(),
-            Container(
-              height: height * 0.9,
-              width: height * 0.9,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: isExpanded == true ? Colors.white : Colors.transparent,
-              ),
-              child: Icon(
-                icon,
-                color: isExpanded == true ? colors[0] : Colors.white,
-                size: height * 0.6,
+            Positioned(
+              left: widget.isExpanded == true
+                  ? (_trueWidth - widget.height * 0.9)
+                  : 0,
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Container(
+                height: widget.height * 0.9,
+                width: widget.height * 0.9,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: widget.isExpanded == true
+                      ? Colors.white
+                      : Colors.transparent,
+                ),
+                child: Icon(
+                  widget.icon,
+                  color: widget.isExpanded == true
+                      ? widget.colors[0]
+                      : Colors.white,
+                  size: widget.height * 0.6,
+                ),
               ),
             ),
           ],
