1df9eff07e1ee783f0c7d8b961378d8f45885732
jixiaoyong <jixiaoyong1995@gmail.com>
2022-04-23 02:12:02 +0800

    refactor: change the struct of initial page

--- lib/components/initial/initial_view.dart
+++ lib/components/initial/initial_view.dart
@@ -9,7 +9,7 @@ import 'package:sirc/widgets/title_text.dart';
 import 'initial_logic.dart';
 
 /*
-* @description: Slide Menu
+* @description: initial page
 *
 * @author: jixiaoyong
 * @email: jixiaoyong1995@gmail.com
@@ -32,114 +32,114 @@ class InitialPage extends StatelessWidget {
         Widget? widget = Center(child: Image.asset(A.assets_ic_launcher));
         if (true == isFirst) {
           // first initial page
-          widget = Stack(
-            children: [
-              PageView.builder(
-                controller: _pageController,
-                itemBuilder: (context, index) {
-                  final item = state.initialTips[index];
-                  return Container(
-                    color: Colors.white,
-                    padding:
-                        EdgeInsets.only(top: 150.dp, right: 20.dp, left: 20.dp),
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          item.first,
-                          height: 300.dp,
-                          fit: BoxFit.cover,
+          widget = SizedBox(
+            width: CommonData.realScreenWidth,
+            child: PageView.builder(
+              controller: _pageController,
+              itemBuilder: (context, index) {
+                final item = state.initialTips[index];
+                return Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.only(
+                    /*  top: 0.2 * screenHeight,*/ right: 20.dp, left: 20.dp),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        item.first,
+                        height: 300.dp,
+                        fit: BoxFit.cover,
+                      ),
+                      SizedBox(
+                        height: 10.dp,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, subIndex) {
+                            return Container(
+                              margin: EdgeInsets.symmetric(horizontal: 5.dp),
+                              height: 10.dp,
+                              width: 10.dp,
+                              decoration: BoxDecoration(
+                                  color: subIndex == index
+                                      ? Colors.deepPurpleAccent
+                                      : Colors.grey,
+                                  borderRadius: BorderRadius.circular(10.dp)),
+                            );
+                          },
+                          itemCount: initialInfoLength,
                         ),
-                        SizedBox(
-                          height: 10.dp,
-                          child: ListView.builder(
-                            shrinkWrap: true,
-                            scrollDirection: Axis.horizontal,
-                            itemBuilder: (context, subIndex) {
-                              return Container(
-                                margin: EdgeInsets.symmetric(horizontal: 5.dp),
-                                height: 10.dp,
-                                width: 10.dp,
-                                decoration: BoxDecoration(
-                                    color: subIndex == index
-                                        ? Colors.deepPurpleAccent
-                                        : Colors.grey,
-                                    borderRadius: BorderRadius.circular(10.dp)),
-                              );
-                            },
-                            itemCount: initialInfoLength,
-                          ),
+                      ),
+                      TitleText(text: item.second),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20.dp),
+                        child: Text(
+                          item.third,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.grey[500],
+                              fontSize: 15.sp,
+                              height: 1.5),
                         ),
-                        TitleText(text: item.second),
-                        Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20.dp),
-                          child: Text(
-                            item.third,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: Colors.grey[500],
-                                fontSize: 15.sp,
-                                height: 1.5),
-                          ),
-                        ),
-                        Container(
-                          child: initialInfoLength - 1 != index
-                              ? GestureDetector(
-                                  onTap: () {
-                                    _pageController.nextPage(
-                                        duration: Duration(milliseconds: 300),
-                                        curve: Curves.easeInOut);
-                                  },
-                                  child: Container(
-                                    height: 50.dp,
-                                    width: 50.dp,
-                                    margin: EdgeInsets.only(top: 30.dp),
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(25.dp),
-                                        gradient: const LinearGradient(colors: [
-                                          Colors.purple,
-                                          Colors.deepPurpleAccent
-                                        ])),
-                                    child: const Icon(
-                                      Icons.arrow_forward_rounded,
-                                      color: Colors.white,
-                                    ),
+                      ),
+                      Container(
+                        child: initialInfoLength - 1 != index
+                            ? GestureDetector(
+                                onTap: () {
+                                  _pageController.nextPage(
+                                      duration: Duration(milliseconds: 300),
+                                      curve: Curves.easeInOut);
+                                },
+                                child: Container(
+                                  height: 50.dp,
+                                  width: 50.dp,
+                                  margin: EdgeInsets.only(top: 30.dp),
+                                  decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.circular(25.dp),
+                                      gradient: const LinearGradient(colors: [
+                                        Colors.purple,
+                                        Colors.deepPurpleAccent
+                                      ])),
+                                  child: const Icon(
+                                    Icons.arrow_forward_rounded,
+                                    color: Colors.white,
                                   ),
-                                )
-                              : GestureDetector(
-                                  onTap: () {
-                                    goMainPage();
-                                  },
-                                  child: Container(
-                                    height: 50.dp,
-                                    width: CommonData.realScreenWidth - 60.dp,
-                                    margin: EdgeInsets.fromLTRB(
-                                        30.dp, 30.dp, 30.dp, 0),
-                                    decoration: BoxDecoration(
-                                        borderRadius:
-                                            BorderRadius.circular(15.dp),
-                                        gradient: const LinearGradient(colors: [
-                                          Colors.purple,
-                                          Colors.deepPurpleAccent
-                                        ])),
-                                    child: Center(
-                                      child: Text(
-                                        "Get Start".tr,
-                                        style: TextStyle(
-                                            color: Colors.white,
-                                            fontSize: 20.sp),
-                                      ),
+                                ),
+                              )
+                            : GestureDetector(
+                                onTap: () {
+                                  goMainPage();
+                                },
+                                child: Container(
+                                  height: 50.dp,
+                                  width: CommonData.realScreenWidth - 60.dp,
+                                  margin: EdgeInsets.fromLTRB(
+                                      30.dp, 30.dp, 30.dp, 0),
+                                  decoration: BoxDecoration(
+                                      borderRadius:
+                                          BorderRadius.circular(15.dp),
+                                      gradient: const LinearGradient(colors: [
+                                        Colors.purple,
+                                        Colors.deepPurpleAccent
+                                      ])),
+                                  child: Center(
+                                    child: Text(
+                                      "Get Start".tr,
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 20.sp),
                                     ),
                                   ),
                                 ),
-                        )
-                      ],
-                    ),
-                  );
-                },
-                itemCount: initialInfoLength,
-              ),
-            ],
+                              ),
+                      )
+                    ],
+                  ),
+                );
+              },
+              itemCount: initialInfoLength,
+            ),
           );
         } else if (false == isFirst) {
           // empty page
