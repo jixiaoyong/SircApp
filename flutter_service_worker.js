b8cbacaaaafe27e2a2fec0d281660ea6ac7968fa
jixiaoyong <jixiaoyong1995@gmail.com>
2022-04-24 13:13:33 +0000

    0b51849 - jixiaoyong(jixiaoyong1995@gmail.com): feat: split complex widget

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,9 +3,9 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "754616feac807107d6e940a5a601e28b",
-"index.html": "b1d5a856e9e1cc2e88f19560d536fbf4",
-"/": "b1d5a856e9e1cc2e88f19560d536fbf4",
+  "main.dart.js": "4cda00cfc70859b115e0a5d8b18ae10f",
+"index.html": "3295b9fb8d461e6a609e9f0d80fc9b44",
+"/": "3295b9fb8d461e6a609e9f0d80fc9b44",
 "favicon.png": "54cf4970601ecd3df8762475dec3f747",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
 "assets/AssetManifest.json": "19cba513af9635eff7d6893ad14445ea",
