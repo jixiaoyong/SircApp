93dd51b63329a542288166e9f316ecd68782adda
jixiaoyong <jixiaoyong1995@gmail.com>
2022-05-31 10:10:41 +0800

    fix: temporarily remove the dependence of UME as they do not work with Flutter 3

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_ume/flutter_ume.dart'; // UME 框架
-import 'package:flutter_ume_kit_console/flutter_ume_kit_console.dart'; // debugPrint 插件包
-import 'package:flutter_ume_kit_device/flutter_ume_kit_device.dart'; // 设备信息插件包
-import 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart'; // 性能插件包
-import 'package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart'; // 代码查看插件包
-import 'package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart'; // UI 插件包
+
+// import 'package:flutter_ume/flutter_ume.dart'; // UME 框架
+// import 'package:flutter_ume_kit_console/flutter_ume_kit_console.dart'; // debugPrint 插件包
+// import 'package:flutter_ume_kit_device/flutter_ume_kit_device.dart'; // 设备信息插件包
+// import 'package:flutter_ume_kit_perf/flutter_ume_kit_perf.dart'; // 性能插件包
+// import 'package:flutter_ume_kit_show_code/flutter_ume_kit_show_code.dart'; // 代码查看插件包
+// import 'package:flutter_ume_kit_ui/flutter_ume_kit_ui.dart'; // UI 插件包
 import 'package:get/get.dart';
 import 'package:sirc/data/languages.dart';
 import 'package:sirc/utils/size_extension.dart' as size_ext;
@@ -27,22 +28,22 @@ void main() {
 
   // LogUtils.forceOpenLog();
 
-  if (kDebugMode) {
-    PluginManager.instance // 注册插件
-      ..register(const WidgetInfoInspector())
-      ..register(const WidgetDetailInspector())
-      ..register(const ColorSucker())
-      ..register(AlignRuler())
-      ..register(Performance())
-      ..register(const ShowCode())
-      ..register(const MemoryInfoPage())
-      ..register(CpuInfoPage())
-      ..register(const DeviceInfoPanel())
-      ..register(Console());
-    runApp(const UMEWidget(child: myApp, enable: true)); // 初始化
-  } else {
-    runApp(myApp);
-  }
+  // if (kDebugMode) {
+  //   PluginManager.instance // 注册插件
+  //     ..register(const WidgetInfoInspector())
+  //     ..register(const WidgetDetailInspector())
+  //     ..register(const ColorSucker())
+  //     ..register(AlignRuler())
+  //     ..register(Performance())
+  //     ..register(const ShowCode())
+  //     ..register(const MemoryInfoPage())
+  //     ..register(CpuInfoPage())
+  //     ..register(const DeviceInfoPanel())
+  //     ..register(Console());
+  //   runApp(const UMEWidget(child: myApp, enable: true)); // 初始化
+  // } else {
+  runApp(myApp);
+  // }
 }
 
 class MyApp extends StatelessWidget {
